e6aa8bf9886fc62247ae12eae4f83dd3abcb054a
MusaKavak <musa0kavak@gmail.com>
2023-03-12 20:06:04 +0300

    Files Tab Styling

--- src/scripts/FileManager.ts
+++ src/scripts/FileManager.ts
@@ -1,8 +1,78 @@
+import FileSvg from "../assets/file.svg";
+import { Socket } from "../connection/Socket";
 import { File } from "../types/File";
+import { Public } from "./Public";
 
 export default class FileManager {
 
+    private filesTab = document.getElementById("files-tab-body")
+    private svg = new FileSvg()
+
     createFiles(file: File) {
-        console.log(file)
+        if (this.filesTab) {
+            this.filesTab.innerHTML = ""
+            this.filesTab.appendChild(this.getHeader(file))
+            this.filesTab.appendChild(this.getBody(file.children))
+        }
+    }
+
+    getBody(children: File[] | undefined): HTMLElement {
+        return Public.createElement({
+            id: "directory-body",
+            children: children?.map(f => this.getRow(f))
+        })
+    }
+
+    getRow(f: File): HTMLElement {
+        const callback = () => this.fileRequest(f.path)
+
+        return Public.createElement({
+            clss: "directory-row card",
+            innerHtml: `
+                <span id="file-icon">${this.getFileIcon()}</span>
+                <span>${f.name || '-'}</span>
+            `,
+            listener: {
+                event: "click",
+                callback
+            }
+        })
+    }
+
+    getHeader(file: File): HTMLElement {
+        return Public.createElement({
+            id: "directory-header",
+            children: [
+                this.getGoBackButton(file.parent),
+                Public.createElement({
+                    id: "directory-name",
+                    content: file.name
+                })
+            ]
+        })
+    }
+
+    getGoBackButton(parent: File | undefined): HTMLElement | undefined {
+        if (parent != undefined) {
+            const callback = () => this.fileRequest(parent.path)
+
+            return Public.createElement({
+                id: "directory-goback",
+                innerHtml: this.svg.goBack,
+                title: parent.name,
+                listener: {
+                    event: "click",
+                    callback
+                }
+            })
+        } else return
+    }
+
+    private getFileIcon(): string {
+        return this.svg.folder
+    }
+
+    private fileRequest(path: string | undefined) {
+        Socket.send("FileRequest", { path })
     }
 }
