bbed1399745f6398fb879e4a005c0ade11c33785
MusaKavak <musa0kavak@gmail.com>
2023-03-25 23:15:03 +0300

    Optimizing SVGs

--- src/assets/notification.svg.ts
+++ src/assets/notification.svg.ts
@@ -2,7 +2,7 @@ export default class NotificationSvg {
 
     folder = `
         <svg class="notification-folder" width="800px" height="800px" viewBox="0 0 24 24" fill="none" >
-            <path id="Vector" d="M16 10L12 14L8 10" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path class="stroke" id="Vector" d="M16 10L12 14L8 10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     `
 }
