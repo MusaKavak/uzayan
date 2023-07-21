3e60ab6734114e8ad3fa75bf83324c8623aff43d
MusaKavak <musa0kavak@gmail.com>
2023-07-21 17:12:22 +0300

    Connection State Container

--- src/connection/ConnectionState.ts
+++ src/connection/ConnectionState.ts
@@ -5,19 +5,96 @@ import { listen } from "@tauri-apps/api/event"
 import { NetworkMessage } from "../types/network/NetworkMessage"
 import { BackendMessage } from "../types/local/BackendMessage"
 import { PairRequest } from "../types/network/PairRequest"
+import Public from "../utils/Public"
 
 export default class ConnectionState {
     static connectedAddress?: string
+    static connectedDevice?: string
 
-    private connectionStateContainer = document.getElementById("connection-state")
     private pairCode = "123414"
+    private connectionStateContainer = document.getElementById("connection-state")
 
     constructor(
         private headerManager: HeaderManager
     ) {
-        this.listenForPair().then(() => this.connectedToLastAddress())
+        this.createConnectionStateElement()
+        this.listenForPair().then()
     }
 
+    private async createConnectionStateElement() {
+        const header = this.connectionStateHeader()
+        const body = await this.connectionStateBody()
+
+        this.connectionStateContainer?.appendChild(header)
+        this.connectionStateContainer?.appendChild(body)
+    }
+
+    private async connectionStateBody(): Promise<HTMLElement> {
+        if (ConnectionState.connectedAddress && ConnectionState.connectedDevice) {
+            return Public.createElement({})
+        } else {
+            const socketAddress = await invoke<SocketAddress>("get_socket_addr")
+
+            return Public.createElement({
+                id: "connection-state-body",
+                children: [
+                    this.qrCode(socketAddress),
+                    this.credentials(socketAddress)
+                ]
+            })
+        }
+    }
+
+    private credentials(socketAddress: SocketAddress): HTMLElement {
+        return Public.createElement({
+            id: "connection-state-body-credentials",
+            children: [
+                this.credential("Address", socketAddress.ip),
+                this.credential("Port", socketAddress.port),
+                this.credential("Code", this.pairCode)
+            ]
+        })
+    }
+
+    private credential(title: string, value: string | number): HTMLElement {
+        return Public.createElement({
+            clss: "credential",
+            innerHtml: `
+                <div class="credential-title">${title}</div>
+                <div class="credential-value">${value}</div>
+            `
+        })
+    }
+
+    private connectionStateHeader(): HTMLElement {
+        if (ConnectionState.connectedAddress && ConnectionState.connectedDevice) {
+            return Public.createElement({})
+        } else {
+            return Public.createElement({
+                id: "connection-state-header",
+                innerHtml: `<span>Enable Secure Connection</span>`,
+                children: [this.secureConnectionCheckbox()]
+            })
+        }
+    }
+
+    private secureConnectionCheckbox(): HTMLElement {
+        const checkbox = document.createElement("input")
+        checkbox.setAttribute("type", "checkbox")
+        checkbox.addEventListener("change", () => {
+            console.log(checkbox.checked)
+        })
+
+        return Public.createElement({
+            type: "label",
+            clss: "switch",
+            id: "secure-connection-checkbox",
+            children: [
+                checkbox,
+                document.createElement("div")
+            ]
+        })
+    }
 
     async listenForPair() {
         invoke("listen_for_pair")
@@ -28,8 +105,6 @@ export default class ConnectionState {
                 this.pair(message.payload.address, networkMessage.payload)
         })
 
-        const udpSocketAddress = await invoke<SocketAddress>("get_socket_addr")
-        this.showQrCode(udpSocketAddress)
     }
 
     private async connectedToLastAddress() {
@@ -45,35 +120,24 @@ export default class ConnectionState {
 
     private async connect(address: string) {
         const isConnected = await invoke<boolean>("connect", { address })
-        console.log(isConnected)
         if (isConnected) {
             ConnectionState.connectedAddress = address
-            this.removeQrCode()
             this.headerManager.sync()
             localStorage.setItem("ConnectedAddress", address)
         }
     }
 
-    private showQrCode(address: SocketAddress) {
-        if (this.connectionStateContainer) {
-            document.body.classList.add("show-connection-state")
-            this.connectionStateContainer.appendChild(this.getQrCode(address))
-            //TODO Add Manual Infotmation
-        }
-    }
-
-    private getQrCode(address: SocketAddress): HTMLElement {
+    private qrCode(address: SocketAddress): HTMLElement {
         const canvas = document.createElement("canvas")
+        canvas.setAttribute("id", "")
         qrcode.toCanvas(
             canvas,
             `http://uzayan-pair?ip=${address.ip}&port=${address.port}&code=${this.pairCode}&name=${address.name}`
         )
-        return canvas
-    }
-
-    private removeQrCode() {
-        document.body.classList.remove("show-connection-state")
-        if (this.connectionStateContainer) this.connectionStateContainer.innerHTML = ""
+        return Public.createElement({
+            id: "connection-state-body-qr",
+            children: [canvas]
+        })
     }
 }
 
